7ff9f3b26ee9a46bd41bf9758da8c9902346379d
ideal_koghli <ideal_koghli@dm.gov.ae>
2024-05-13 15:01:03 +0400

    feat(auth): add rules eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,15 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100],
+  },
+}
--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,31 @@ export default nuxt(
     {
       unocss: true,
       formatters: true,
+      rules: {
+        'vue/component-name-in-template-casing': ['error', 'PascalCase', { registeredComponentsOnly: false, ignores: [] }],
+        'vue/component-api-style': ['error', ['script-setup']],
+        'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+        'vue/block-order': ['error', {
+          order: ['script', 'template', 'style'],
+        }],
+        'vue/define-emits-declaration': ['error', 'type-based'],
+        'vue/define-props-declaration': ['error', 'type-based'],
+        'vue/define-macros-order': ['error', {
+          order: ['defineProps', 'defineEmits'],
+          defineExposeLast: true,
+        }],
+         "vue/enforce-style-attribute": [
+          "error",
+          { "allow": ["scoped"] }
+        ],
+        'vue/html-button-has-type': ['error', { button: true, submit: true, reset: true }],
+        'vue/no-empty-component-block': ['error'],
+        'vue/no-multiple-objects-in-class': ['error'],
+        'vue/v-for-delimiter-style': ['error', 'in'],
+        'vue/v-on-handler-style': ['error', ["method", "inline-function"]],
+        'vue/no-side-effects-in-computed-properties': ['error'],
+        "no-magic-numbers": ["error", { "ignore": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }],
+      },
     },
   ),
 )
