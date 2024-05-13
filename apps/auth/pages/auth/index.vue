9cff82a324644ed3f02d082193a8c39923a1e927
ideal_koghli <ideal_koghli@dm.gov.ae>
2024-05-13 08:20:59 +0400

    updated

---
+++ apps/auth/pages/auth/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Auth Here
+  </div>
+</template>
+
+<style scoped></style>
---
+++ apps/landing/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: 'Home',
+})
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <Button label="Hi Home" />
+  </div>
+</template>
