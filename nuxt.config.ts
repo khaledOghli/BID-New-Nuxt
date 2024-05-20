d59fb4c36dc702aa664d9446060109d971d04c5e
ideal_koghli <ideal_koghli@dm.gov.ae>
2024-05-20 07:35:09 +0400

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
-import process from 'node:process'
-import { fileURLToPath } from 'node:url'
+import process from 'node:process';
+import { fileURLToPath } from 'node:url';
 // import { pwa } from './config/pwa'
-import { appDescription } from './constants/index'
+import { appDescription } from './constants/index';
 
-const baseUrl = process.env.BASE_URL || '/'
+const baseUrl = process.env.BASE_URL || '/';
 // eslint-disable-next-line no-magic-numbers
-const oneYear = 1000 * 60 * 60 * 24 * 365
+const oneYear = 1000 * 60 * 60 * 24 * 365;
 
 export default defineNuxtConfig({
   extends: ['./UI', './apps/landing', './apps/dashboard', './apps/auth'],
@@ -20,7 +20,6 @@ export default defineNuxtConfig({
     // '@vite-pwa/nuxt',
     '@nuxt/eslint',
     'nuxt-primevue',
-    '@nuxt/content',
     'nuxt-icon',
     '@nuxtjs/i18n',
     '@nuxtjs/fontaine',
@@ -53,11 +52,11 @@ export default defineNuxtConfig({
 
   viewport: {
     breakpoints: {
-      'xs': 320,
-      'sm': 640,
-      'md': 768,
-      'lg': 1024,
-      'xl': 1280,
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
       '2xl': 1536,
     },
 
@@ -123,7 +122,7 @@ export default defineNuxtConfig({
     prerender: {
       crawlLinks: false,
       routes: ['/'],
-      ignore: ['/hi'],
+      ignore: ['/test'],
     },
   },
 
@@ -262,4 +261,4 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+});
